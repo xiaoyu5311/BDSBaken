cdc9833b7bfe8a5a816cc13cf143770c2bac813b
xiaoyu <3157575603@qq.com>
2024-05-14 09:34:45 +0800

    init

---
+++ src/main/java/com/xiaoyu/bdsbaken/BdsBakenApplication.java
@@ -0,0 +1,15 @@
+package com.xiaoyu.bdsbaken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdsBakenApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BdsBakenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaoyu/bdsbaken/BdsBakenApplicationTests.java
@@ -0,0 +1,15 @@
+package com.xiaoyu.bdsbaken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdsBakenApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
